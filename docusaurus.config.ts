3a80a488120b6845e68b6d6f7dcd95120138a046
Wantao Liu <lwantaoo@qq.com>
2025-02-12 03:30:14 +0800

    chore: change the production url of site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/logo-black-48px.png',
 
   // Set the production url of your site here
-  url: 'https://ee-wiki.github.io',
+  url: 'https://ee-wiki.vantao.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/portal-nextgen',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
