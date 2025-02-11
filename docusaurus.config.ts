135157636f588ab90f65a3b484d8a7b81df7ac9b
Wantao Liu <lwantaoo@qq.com>
2025-02-12 03:20:41 +0800

    fix: change the production url of site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/logo-black-48px.png',
 
   // Set the production url of your site here
-  url: 'https://ee-wiki.tidalforce.cn',
+  url: 'https://ee-wiki.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/portal-nextgen',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
