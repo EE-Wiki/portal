ba8e6f387a89e73d397c75efaa6c3b05fe3af6a7
Wantao Liu <lwantaoo@qq.com>
2025-02-12 13:49:55 +0800

    fix: change editUrl to correct form

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ee-wiki/portal-nextgen/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ee-wiki/portal-nextgen/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ee-wiki/portal-nextgen/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ee-wiki/portal-nextgen/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
